3929a959fb455490a1b12fe135f2af023968f68c
yuyin_zhang <yuyin_zhang@realsil.com.cn>
2018-03-04 14:57:09 +0800

    [ToolBox]plot pcm data, ok. but still have some problem on data process

--- ToolBox/SerialAssit/serialassit.cpp
+++ ToolBox/SerialAssit/serialassit.cpp
@@ -47,8 +47,11 @@ SerialAssit::SerialAssit(QWidget *parent) : QWidget(parent)
     /* 2. 曲线参数设置 */
     gRevDataLen = 0;
     pPlot = new QCustomPlot();
-    pPlot->xAxis->setVisible(false);
-    pPlot->yAxis->setVisible(false);
+    pPlot->xAxis->setVisible(true);
+    pPlot->xAxis2->setVisible(true);
+    pPlot->yAxis->setVisible(true);
+    pPlot->yAxis2->setVisible(true);
+    pPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
 
     /* 页面布局设置 */
     QHBoxLayout *pLayout01 = new QHBoxLayout();
@@ -155,24 +158,38 @@ void SerialAssit::comfresh()
             return ;
         }
 
-//        pPlot->addGraph();
-//        pPlot->graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
-//        pPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20)));
+        pPlot->addGraph();
+        pPlot->graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
+        pPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20)));
 
-//        QByteArray arr;
-//        arr = inputFile->readAll();
-//        for(int i=0;i<inputFile->size();i++)
-//        {
-//            pPlot->graph(0)->addData(i, arr[i]);
-//        }
+        QByteArray arr;
+        arr = inputFile->readAll();
+        quint64 len = inputFile->size();
+        for(int i=0;i<len;i+=2)
+        {
+            //QString str;
+            qint32 i0;
+            //data0 = (arr[i] + arr[i+1]<<8);
 
-//        pPlot->graph(0)->rescaleAxes();
-//        pPlot->replot();
+            i0 = arr[i+1];
+            i0 = ((i0<<8) + arr[i]);
+            if (i0 & 0x8000)
+                ;//i0 = 0 - i0;
+//            if (i0 > 0)
+//                qDebug()<<"<1--->"+ QString::number(i0) ;
+//            else
+//                qDebug()<<"<-2-->" + QString::number(i0);
+
+            pPlot->graph(0)->addData(i, i0);
+        }
+
+        pPlot->graph(0)->rescaleAxes();
+        pPlot->replot();
 
         //设置采样格式
         QAudioFormat audioFormat;
         //设置采样率
-        audioFormat.setSampleRate(8000);
+        audioFormat.setSampleRate(16000);
         //设置通道数
         audioFormat.setChannelCount(1);
         //设置采样大小，一般为8位或16位
